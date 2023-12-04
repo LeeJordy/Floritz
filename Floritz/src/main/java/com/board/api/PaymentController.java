0bbe7cdd3cda16729aa890a394791794194f66a2
LeeJordy <o3o_jung@naver.com>
2023-12-04 21:30:00 +0900

    2023.11.28. Floritz

---
+++ Floritz/src/main/java/com/board/api/PaymentController.java
@@ -0,0 +1,32 @@
+package com.board.api;
+
+import java.io.IOException;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
+
+import com.siot.IamportRestClient.IamportClient;
+import com.siot.IamportRestClient.exception.IamportResponseException;
+import com.siot.IamportRestClient.response.IamportResponse;
+import com.siot.IamportRestClient.response.Payment;
+
+@Controller
+public class PaymentController {
+
+    private final IamportClient iamportClient;
+
+    public PaymentController() {
+        this.iamportClient = new IamportClient("impid",
+                "imp");
+    }
+
+    @ResponseBody
+    @RequestMapping("/verify/{impid}")
+    public IamportResponse<Payment> paymentByImpUid(@PathVariable("impid") String imp_uid)
+            throws IamportResponseException, IOException {
+        return iamportClient.paymentByImpUid(imp_uid);
+    }
+
+}
